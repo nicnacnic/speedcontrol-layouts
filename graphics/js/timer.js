7caae396c38fac65009ac999e1f52995d395df40
nicnacnic <nicnacnicyt@gmail.com>
2021-05-25 00:40:14 -0400

    Fixed broken timer

--- graphics/js/timer.js
+++ graphics/js/timer.js
@@ -1,14 +1,17 @@
+$(() => {
+	const timer = nodecg.Replicant('timer', 'nodecg-speedcontrol');
+	let timerElem = $('#timer');
 
-var speedcontrolBundle = 'nodecg-speedcontrol';
-var timerElem = $('#timer');
-var timer = nodecg.Replicant('timer', speedcontrolBundle);
-timer.on('change', (newVal, oldVal) => {
-	if (newVal)
-		updateTimer(newVal, oldVal);
+	NodeCG.waitForReplicants(timer).then(() => {
+		timer.on('change', (newVal, oldVal) => {
+			console.log(timer);
+			updateTimer(newVal, oldVal);
+		});
+	})
+
+	function updateTimer(newVal, oldVal) {
+		if (oldVal) timerElem.toggleClass('timer_' + oldVal.state, false);
+		timerElem.toggleClass('timer_' + newVal.state, true);
+		timerElem.html(newVal.time);
+	}
 });
-
-function updateTimer(newVal, oldVal) {
-	if (oldVal) timerElem.toggleClass('timer_' + oldVal.state, false);
-	timerElem.toggleClass('timer_' + newVal.state, true);
-	timerElem.html(newVal.time);
-}
