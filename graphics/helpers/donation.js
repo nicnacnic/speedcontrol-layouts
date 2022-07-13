59197a178902f229c93ed100c703509475004629
nicnacnic <nicnacnicyt@gmail.com>
2022-07-13 15:30:49 -0400

    Update donation.js

--- graphics/helpers/donation.js
+++ graphics/helpers/donation.js
@@ -4,12 +4,12 @@ let countUp;
 
 NodeCG.waitForReplicants(data).then(() => {
 
-	data.on('change', (newVal) => handleCountUp(newVal.event.total));
+	data.on('change', (newVal) => handleCountUp(newVal.event.total, newVal.event.currency));
 
-	function handleCountUp(amount) {
+	function handleCountUp(amount, currency) {
 		if (!countUp) {
 			countUp = new CountUp('donationTotal', amount, amount, 0, 0.75, {
-				prefix: '$'
+				prefix: currency,
 			});
 			countUp.start();
 		} else countUp.update(amount);
